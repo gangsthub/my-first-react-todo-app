66463bb5acb575ce17c9c7f36fee9839c13a95f0
paul_melero <paulmelero@gmail>
2019-03-01 02:05:24 +0100

    [master] trigger new deploy

--- src/scripts/components/toDos/logging/History.jsx
+++ src/scripts/components/toDos/logging/History.jsx
@@ -1,17 +1,13 @@
 // @ts-check
 
-import React/* , {useRef} */ from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import HistoryItem from './HistoryItem'
 
 const History = ({todos}) => {
-  // const scrollToBottomEl = useRef()
-  // const currentInstanceEl = scrollToBottomEl && scrollToBottomEl.current
-  // if (currentInstanceEl) currentInstanceEl.scrollTop = currentInstanceEl.scrollHeight
-
   return (
-    <div className="pa-3" /* ref={scrollToBottomEl} */>
+    <div className="pa-3">
       {
         todos.map((todo, i) => <HistoryItem key={i} todo={todo} />)
       }
